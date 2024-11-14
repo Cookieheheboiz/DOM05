07513909b15da8279dec0e8d846ee5eb50bdbda3
hungtrannhatt <23020543@vnu.edu.vn>
2024-11-14 18:51:58 +0700

    Revert "14_11_2024"

--- BTL_OOP/LibraryManagement/src/main/java/com/librarymanagement/MenuController.java
+++ BTL_OOP/LibraryManagement/src/main/java/com/librarymanagement/MenuController.java
@@ -1,27 +1,20 @@
 package com.librarymanagement;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.io.IOException;
-
 public class MenuController {
 
-    public Button SearchBook;
     @FXML
     private Button Borrowmn;
 
     @FXML
-    private Button Storagemn;
+    private Button storagemn;
 
     @FXML
     private Button GameField;
@@ -48,48 +41,9 @@ public class MenuController {
     }
 
     public void LogoutAction() {
-        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-        alert.setHeaderText(null);
-        alert.setContentText("Are you sure you want to exit?");
-        if (alert.showAndWait().get() == ButtonType.OK) {
-            Stage stage = (Stage) LogoutField.getScene().getWindow();
-            stage.close();
-            Logout();
-        }
+        Stage stage = (Stage) LogoutField.getScene().getWindow();
+        stage.close();
+        Logout();
     }
 
-    public void AddStorage() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/librarymanagement/fxml/category.fxml")); // Updated to category.fxml
-            AnchorPane nextPage = loader.load(); // Load the AnchorPane from category.fxml
-
-            // Get the current stage
-            Stage stage = (Stage) Storagemn.getScene().getWindow();
-
-            // Set the new scene with the loaded AnchorPane
-            Scene scene = new Scene(nextPage);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-    }
-
-    public void FindBook() {
-        try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/librarymanagement/fxml/search-view.fxml")); // Updated to category.fxml
-            AnchorPane nextPage = loader.load(); // Load the AnchorPane from category.fxml
-
-            // Get the current stage
-            Stage stage = (Stage) SearchBook.getScene().getWindow();
-
-            // Set the new scene with the loaded AnchorPane
-            Scene scene = new Scene(nextPage);
-            stage.setScene(scene);
-            stage.show();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
 }
