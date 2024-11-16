11cc729310026434176c918b1c91905561cd407a
hungtrannhatt <powerhung1@gmail.com>
2024-11-16 11:14:15 +0700

    category_search

--- BTL_OOP/LibraryManagement/src/main/java/com/librarymanagement/DatabaseConnection.java
+++ BTL_OOP/LibraryManagement/src/main/java/com/librarymanagement/DatabaseConnection.java
@@ -10,7 +10,7 @@ public class DatabaseConnection {
     public static Connection getConnection() {
         String databaseName = "librarymanagement";
         String databaseUser = "root";
-        String databasePassword = "chitogeABVs32";
+        String databasePassword = "123456";
         String url = "jdbc:mysql://localhost/" + databaseName;
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
